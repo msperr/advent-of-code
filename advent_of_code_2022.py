f918e5f746ecb5f3f799ebafa9714ea4573dbdae
Martin Sperr <sperr@galor.de>
2022-12-02 06:54:10 +0100

    Day 2

--- advent_of_code_2022.py
+++ advent_of_code_2022.py
@@ -1,10 +1,17 @@
-DAY = 1
+DAY = 2
 PART = 2
 
 if __name__ == '__main__':
-    with open(f'input_2021/day_{DAY}_input.txt') as file:
+    with open(f'input_2022/day_{DAY}_input.txt') as file:
         lines = file.readlines()
     if DAY == 1:
         idx = [-1] + [i for i, s in enumerate(lines) if s.strip() == ''] + [len(lines)]
         cals = sorted([sum(int(t.strip()) for t in lines[i1 + 1:i2]) for i1, i2 in zip(idx[:-1], idx[1:])])
         print(sum(c for c in cals[(-1 if PART == 1 else -3):]))
+    if DAY == 2:
+        lines = [s.strip().split(' ') for s in lines]
+        d0, d1 = ({s: i for i, s in enumerate(c)} for c in [list('ABC'), list('XYZ')])
+        if PART == 1:
+            print(sum((d1[s[1]] - d0[s[0]] + 1) % 3 * 3 + d1[s[1]] + 1 for s in lines))
+        else:
+            print(sum(d1[s[1]] * 3 + (d0[s[0]] + d1[s[1]] - 1) % 3 + 1 for s in lines))
