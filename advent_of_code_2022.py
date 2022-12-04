c0f8e9a2951fd7383cb8549d81ae264bf39b4548
Martin Sperr <sperr@galor.de>
2022-12-04 09:23:12 +0100

    Day 3, 4

--- advent_of_code_2022.py
+++ advent_of_code_2022.py
@@ -1,5 +1,10 @@
-DAY = 2
+DAY = 4
 PART = 2
+
+
+def letter_index(s):
+    return ord(s) - ord('A') + 27 if ord(s) < ord('a') else ord(s) - ord('a') + 1
+
 
 if __name__ == '__main__':
     with open(f'input_2022/day_{DAY}_input.txt') as file:
@@ -15,3 +20,13 @@ if __name__ == '__main__':
             print(sum((d1[s[1]] - d0[s[0]] + 1) % 3 * 3 + d1[s[1]] + 1 for s in lines))
         else:
             print(sum(d1[s[1]] * 3 + (d0[s[0]] + d1[s[1]] - 1) % 3 + 1 for s in lines))
+    if DAY == 3:
+        if PART == 1:
+            print(sum(letter_index([s for s in t[:int(len(t) / 2)] if s in t[int(len(t) / 2):]][0]) for t in lines))
+        else:
+            print(sum(letter_index([s for s in lines[i] if s in lines[i + 1] and s in lines[i + 2]][0]) for i in
+                      range(0, len(lines), 3)))
+    if DAY == 4:
+        lines = [[[int(u) for u in t.split('-')] for t in s.strip().split(',')] for s in lines]
+        print(sum(1 for s in lines if any(
+            t1[0] <= t2[0 if PART == 1 else 1] and t2[1 if PART == 1 else 0] <= t1[1] for t1, t2 in zip(s, s[::-1]))))
